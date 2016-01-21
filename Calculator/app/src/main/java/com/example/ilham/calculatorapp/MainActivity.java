212168c28a75670fc34a0ed8db7de6d02109814a
Nesreen Malkawi <nesreen.malkawi@yahoo.com>
2016-01-21 12:47:21 +0100

    Minor changes gradle

--- Calculator/app/src/main/java/com/example/ilham/calculatorapp/MainActivity.java
+++ Calculator/app/src/main/java/com/example/ilham/calculatorapp/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.TextView;
 import java.security.PrivateKey;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-   private   Button btnAdd,btnSub,btnMult,btnDiv;
-   private   EditText etFirst, etSecond;
+    private Button btnAdd, btnSub, btnMult, btnDiv;
+    private EditText etFirst, etSecond;
     private TextView tvResult;
 
     @Override
@@ -22,49 +22,50 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         init();
     }
-   private   void init() {
-         btnAdd= (Button) findViewById(R.id.btnAdd);
-         btnSub= (Button) findViewById(R.id.btnSub);
-         btnMult= (Button) findViewById(R.id.btnMult);
-         btnDiv= (Button) findViewById(R.id.btnDiv);
-         etFirst= (EditText) findViewById(R.id.etFirstNumber);
-         etSecond= (EditText)findViewById(R.id.etSecondNumber);
-         tvResult= (TextView)findViewById(R.id.tvResult);
 
-       btnAdd.setOnClickListener(this);
-       btnSub.setOnClickListener(this);
-       btnMult.setOnClickListener(this);
-       btnDiv.setOnClickListener(this);
+    private void init() {
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        etFirst = (EditText) findViewById(R.id.etFirstNumber);
+        etSecond = (EditText) findViewById(R.id.etSecondNumber);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
-        String num1= etFirst.getText().toString();
-        String num2= etSecond.getText().toString();
+        String num1 = etFirst.getText().toString();
+        String num2 = etSecond.getText().toString();
         switch (view.getId()) {
             case R.id.btnAdd:
-                int addition = Integer.parseInt(num1)+ Integer.parseInt(num2);
+                int addition = Integer.parseInt(num1) + Integer.parseInt(num2);
                 tvResult.setText(String.valueOf(addition));
                 break;
             case R.id.btnSub:
-                int subtraction = Integer.parseInt(num1)- Integer.parseInt(num2);
+                int subtraction = Integer.parseInt(num1) - Integer.parseInt(num2);
                 tvResult.setText(String.valueOf(subtraction));
                 break;
             case R.id.btnMult:
                 try {
-                int multiplication = Integer.parseInt(num1)* Integer.parseInt(num2);
-                tvResult.setText(String.valueOf(multiplication));}
-                catch (Exception e) {
-                tvResult.setText("Cannot be Multiply");
+                    int multiplication = Integer.parseInt(num1) * Integer.parseInt(num2);
+                    tvResult.setText(String.valueOf(multiplication));
+                } catch (Exception e) {
+                    tvResult.setText("Cannot be Multiply");
                 }
                 break;
             case R.id.btnDiv:
-                try{
-                int division = Integer.parseInt(num1)/ Integer.parseInt(num2);
-                tvResult.setText(String.valueOf(division));}
-                catch (Exception e) {
-                tvResult.setText("Cannot be Divid!");
+                try {
+                    int division = Integer.parseInt(num1) / Integer.parseInt(num2);
+                    tvResult.setText(String.valueOf(division));
+                } catch (Exception e) {
+                    tvResult.setText("Cannot be Divid!");
                 }
                 break;
 
